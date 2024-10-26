03710a30e538af9d72de2f08e4117988ad757fac
alicee <avega2003@yandex.ru>
2024-10-26 10:10:41 +0300

    windows script

--- server_side/script.sh
+++ server_side/script.sh
@@ -1,2 +1,7 @@
+# MacOS & Linux
 docker build -t my_fastapi_app .
 docker run -d -v /Users/alicee/Desktop/Work_Main/docker_test/server_side:/app/data -p 8000:8000 my_fastapi_app
+
+# Windows PowerShell
+#docker build -t my_fastapi_app .
+#docker run -d -v C:\Users\alicee\Desktop\Work_Main\docker_test\server_side:/app/data -p 8000:8000 my_fastapi_app
